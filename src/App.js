f51c86593f04714571323e475c5aac93d3092c36
Pavel <cemenru@mail.ru>
2022-08-25 11:54:00 +0300

    lesson-33

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Card from './components/elements/card';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="container">
+        <header className="header">
+          <h1 className="header__title">наша продукция</h1>
+          <div className="header__basket basket">
+            <div className="basket__quantity quantity">
+              <div className="quantity__position">3 товара</div>
+              <div className="quantity__sum">на сумму 3 500 ₽</div>
+
+            </div>
+            <div className="basket__icon">
+              <img src="images/basket.svg" alt="" />
+            </div>
+          </div>
+        </header>
+        <div className="cards">
+        <Card 
+        url={'images/1.png'}
+        title={'Устрицы по рокфеллеровски'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'2 700'}
+        weight={'500 г.'}/>
+
+        <Card 
+        url={'images/2.png'}
+        title={'Свиные ребрышки на гриле с зеленью'}
+        text={'Не следует, однако забывать, что реализация намеченных плановых'}
+        price={'1 600'}
+        weight={'750 г.'}/>
+
+        <Card 
+        url={'images/3.png'}
+        title={'Креветки по-королевски в лимонном соке'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'1 820'}
+        weight={'7 шт.'}/>
+
+        <Card 
+        url={'images/4.png'}
+        title={'Устрицы по рокфеллеровски'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'2 700'}
+        weight={'500 г.'}/>
+
+        <Card 
+        url={'images/1.png'}
+        title={'Устрицы по рокфеллеровски'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'2 700'}
+        weight={'500 г.'}/>
+
+        <Card 
+        url={'images/2.png'}
+        title={'Свиные ребрышки на гриле с зеленью'}
+        text={'Не следует, однако забывать, что реализация намеченных плановых'}
+        price={'1 600'}
+        weight={'750 г.'}/>
+
+        <Card 
+        url={'images/3.png'}
+        title={'Креветки по-королевски в лимонном соке'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'1 820'}
+        weight={'7 шт.'}/>
+
+        <Card 
+        url={'images/4.png'}
+        title={'Устрицы по рокфеллеровски'}
+        text={'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры'}
+        price={'2 700'}
+        weight={'500 г.'}/>
+        </div>
+
+      </div>
+    </main>
   );
 }
 
